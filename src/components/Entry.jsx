958455608fae72e9abcf13669ed9de3cb7b8c0dc
Alex <alex_s11p@abv.bg>
2020-10-26 14:29:54 +0000

    Emojipedia React mapping practice

---
+++ src/components/Entry.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+
+function Entry (props) {
+  return ( 
+  <div className="term">
+<dt>
+  <span className="emoji" role="img" aria-label={props.emoji}>
+    💪
+  </span>
+  <span>{props.name}</span>
+</dt>
+<dd>
+  {props.meaning}
+</dd>
+</div>
+
+);
+
+}
+
+export default Entry;
